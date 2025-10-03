fb0e1248bda484afd79d6c3a2f57f768c95f423d
sasaki <119950722+hutamisiina@users.noreply.github.com>
2025-10-03 23:39:57 +0900

    made wadoldy class

---
+++ python_std/study.py
@@ -0,0 +1,12 @@
+class wadoldy:
+    def __init__(self, name):
+        self.name = name
+
+    def study(self):
+        print(f"{self.name} is studying.")
+    def play(self):
+        print(f"{self.name} is playing.")
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
+    def eat(self):
+        print(f"{self.name} is eating.")            
