e175d7b280b97e166544958ff86c2471c656d884
sasaki <119950722+hutamisiina@users.noreply.github.com>
2025-10-09 19:36:41 +0900

    Update  class.py

--- python_std/ class.py
+++ python_std/ class.py
@@ -1,8 +1,11 @@
-class hogehoge:
-    def __init__(self, name=int(input()):
+class Name:
+    def __init__(self, name):
         self.name = name
     def __str__(self):
         return self.name
-class hoge(hogehoge):
-    pass
+class Add_Name(Name):
+    def add_name(self):
+        add_name = input()
+        return self.name
+        
     
