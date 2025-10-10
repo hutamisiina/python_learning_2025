98edb74812e6d7a77d27c21b8c02ac6eca4d09c8
sasaki <119950722+hutamisiina@users.noreply.github.com>
2025-10-10 23:14:52 +0900

    Update  class.py

--- python_std/ class.py
+++ python_std/ class.py
@@ -3,9 +3,14 @@ class Name:
         self.name = name
     def __str__(self):
         return self.name
-class Add_Name(Name):
     def add_name(self):
         add_name = input()
-        return self.name
+        new_name = add_name + self.name
+        return add_name
+
+n = Name("wanya")
+print(n)
+n.add_name()
+
         
     
