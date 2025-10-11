df1d02c19edec3a2a40b66e20897d0fa8e3fc024
sasaki <119950722+hutamisiina@users.noreply.github.com>
2025-10-11 23:58:14 +0900

    Update  class.py

--- python_std/ class.py
+++ python_std/ class.py
@@ -1,16 +1,16 @@
 class Name:
     def __init__(self, name):
         self.name = name
+    
     def __str__(self):
         return self.name
+    
     def add_name(self):
-        add_name = input()
-        new_name = add_name + self.name
-        return add_name
+        add_name = input("追加する名前を入力: ")
+        self.name = add_name + self.name  # self.nameを更新
+        return self.name  # 更新された名前を返す
 
 n = Name("wanya")
-print(n)
-n.add_name()
-
-        
-    
+print(n)  # 出力: wanya
+n.add_name()  # 名前を更新
+print(n)  # 更新された名前を出力
